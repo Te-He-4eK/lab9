8a766c33cc7bcb0879dbab9d215de31fc90bfeda
Руслан <@LEGENDA>
2023-04-17 22:08:50 +0300

    Добавьте файлы проекта.

---
+++ lab9-2/lab9-2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[] arr = new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        Console.WriteLine("Массив до изменения:");
+        foreach (var item in arr)
+        {
+            Console.Write($"{item} ");
+        }
+        Console.WriteLine();
+
+        EnterPartOfArray(arr, 2, 7);
+
+        Console.WriteLine("Массив после ввода:");
+        foreach (var item in arr)
+        {
+            Console.Write($"{item} ");
+        }
+        Console.WriteLine();
+
+        FillPartOfArrayWithRandom(arr, 1, 8);
+
+        Console.WriteLine("Массив после заполнения случайными числами:");
+        foreach (var item in arr)
+        {
+            Console.Write($"{item} ");
+        }
+        Console.WriteLine();
+    }
+
+    static void EnterPartOfArray(double[] arr, int k1, int k2)
+    {
+        for (int i = k1; i <= k2; i++)
+        {
+            Console.Write($"Введите элемент {i} массива: ");
+            arr[i] = double.Parse(Console.ReadLine());
+        }
+    }
+
+    static void FillPartOfArrayWithRandom(double[] arr, int k1, int k2)
+    {
+        Random rand = new Random();
+        for (int i = k1; i <= k2; i++)
+        {
+            arr[i] = rand.NextDouble();
+        }
+    }
+}
---
+++ lab9/lab9-1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static double Calculate(double x)
+    {
+        int a = new Random().Next(-50, 50);
+        if (x < 0)
+        {
+            return x + Math.Pow(Math.Sin(1 / x - a) + 4, 2);
+        }
+        else if (a * a > x * x) 
+        {
+            return a * x / Math.Sqrt(Math.Pow(a, 2) - Math.Pow(x, 2));
+        }
+        else
+        {
+            throw new ArgumentException("Invalid input value: division by zero");
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Calculate(-1) = " + Calculate(-1)); 
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+}
